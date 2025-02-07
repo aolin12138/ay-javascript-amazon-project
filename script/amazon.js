4feee9c2ccf0f20305a47fe213a55b69527be2fd
ayang <ayan607@aucklanduni.ac.nz>
2025-02-07 14:07:41 +1300

    stored item data in a list

---
+++ script/amazon.js
@@ -0,0 +1,28 @@
+const products = [{
+  image: 'athletic-cotton-socks-6-pairs',
+  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+  stars: {
+    rating: 4.5,
+    count: 87
+  },
+  priceCents: 1090
+},
+{
+  image: 'intermediate-composite-basketball',
+  name: 'Intermediate Size Basketball',
+  stars: {
+    rating: 4.0,
+    count: 127
+  },
+  priceCents: 2095
+},
+{
+  image: 'adults-plain-cotton-tshirt-2-pack-teal',
+  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
+  stars: {
+    rating: 4.5,
+    count: 56
+  },
+  priceCents: 799
+}
+]
