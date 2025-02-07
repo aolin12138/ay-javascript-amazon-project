1f08831787345aa1d189384298bbb34ae00001be
ayang <ayan607@aucklanduni.ac.nz>
2025-02-07 14:49:16 +1300

    replaced the item list with the prepared one and added the js file for cart

--- script/amazon.js
+++ script/amazon.js
@@ -1,39 +1,10 @@
-const products = [{
-  image: 'athletic-cotton-socks-6-pairs',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  stars: {
-    rating: 4.5,
-    count: 87
-  },
-  priceCents: 1090
-},
-{
-  image: 'intermediate-composite-basketball',
-  name: 'Intermediate Size Basketball',
-  stars: {
-    rating: 4.0,
-    count: 127
-  },
-  priceCents: 2095
-},
-{
-  image: 'adults-plain-cotton-tshirt-2-pack-teal',
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  stars: {
-    rating: 4.5,
-    count: 56
-  },
-  priceCents: 799
-}
-]
-
 let html = ''
 
 products.forEach(product => { 
   html += `<div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
-              src="images/products/${product.image}.jpg">
+              src = ${product.image}>
           </div>
 
           <div class="product-name limit-text-to-2-lines">
@@ -42,14 +13,14 @@ products.forEach(product => {
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${product.stars.rating * 10}.png">
+              src="images/ratings/rating-${product.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
-              ${product.stars.count}
+              ${product.rating.count}
             </div>
           </div>
 
           <div class="product-price">
-            $${product.priceCents / 100}
+            $${(product.priceCents / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
@@ -74,7 +45,8 @@ products.forEach(product => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary"
+          data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>
